92cd3107c3e89d993c36464c4f5c8f6b6d7bb864
Summer <su943515688@gmail.com>
2020-03-24 17:08:46 +0800

    [+] 用户注册存入数据库

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "http://127.0.0.1:9090",
+  baseURL: "http://127.0.0.1:9080",
   timeout: 10000,
   headers: {"Content-Type": "application/json;charset=UTF-8"}
 });
