f01f4c9a6272b15c6564e249a09a98859c01da23
Summer <su943515688@gmail.com>
2020-03-27 23:01:21 +0800

    [*] 问答修改

--- src/views/ask/index.vue
+++ src/views/ask/index.vue
@@ -3,19 +3,19 @@
     <Card title="提问">
       <Form label-position="top" :model="form">
         <FormItem prop="title" label="标题">
-          <Input type="text" v-model="form.title" placeholder="请输入标题" />
+          <Input type="text" v-model="form.caption" placeholder="请输入标题"/>
         </FormItem>
         <FormItem prop="category" label="类别">
-          <Select v-model="form.category" placeholder="请选择类别">
-            <Option value="1">技术</Option>
-            <Option value="2">生活</Option>
-            <Option value="3">搞笑</Option>
+          <Select v-model="form.category_id" placeholder="请选择类别">
+            <Option v-for="option in categoryList" :value="option.category_id" :key="option.id">
+              {{ option.category_name }}
+            </Option>
           </Select>
         </FormItem>
         <FormItem prop="centent" label="内容">
           <Input
             type="textarea"
-            v-model="form.centent"
+            v-model="form.content"
             placeholder="请输入内容"
           />
         </FormItem>
@@ -26,16 +26,42 @@
 </template>
 
 <script>
-export default {
-  name: "Ask",
-  data() {
-    return {
-      form: {
-        title: ""
+  export default {
+    name: "Ask",
+    data() {
+      return {
+        loading: false,
+        form: {
+          caption: "",
+          category_id: "",
+          content: "",
+        },
+        categoryList: [],
       }
-    };
-  }
-};
+    },
+    created() {
+      this.getCategoryList();
+    },
+    methods: {
+      async getCategoryList() {
+        let result = await this.$http.get("/api/category/list");
+        if (result.data.code === 0) {
+          this.categoryList = result.data.data;
+        } else {
+          this.$Message.error(result.data.msg);
+        }
+      },
+      async submit() {
+        let result = await this.$http.post("/api/question/submit", this.form);
+        if (result.data.code === 0) {
+          this.$Message.success("添加成功");
+          await this.$router.push("/");
+        } else {
+          this.$Message.error(result.data.msg)
+        }
+      }
+    }
+  };
 </script>
 
 <style lang="scss" scoped></style>
