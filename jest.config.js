df02d430a058531339f405c0a0b190541dd92a21
Summer <su943515688@gmail.com>
2020-04-14 23:02:57 +0800

    [!] 解决golang 循环引用的问题

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   transform: {
     "^.+\\.vue$": "vue-jest",
     ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$":
-      "jest-transform-stub",
+      "jest-transform-base",
     "^.+\\.jsx?$": "babel-jest"
   },
   moduleNameMapper: {
